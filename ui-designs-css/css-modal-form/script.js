d271aa6b71e7811b1fb9b52b7da0a99d40ec29fa
Hafidz <halfpheez92@gmail.com>
2019-05-21 17:13:32 +0800

    added comments.

--- ui-designs-css/css-modal-form/script.js
+++ ui-designs-css/css-modal-form/script.js
@@ -1,6 +1,7 @@
 // JS & Jquery implementations.
 
 $(function () {
+
     const signupModal = $('.signup-form-wrapper');
     const loginModal = $('.login-form-wrapper');
     const signupBtn = $('.signup-btn');
@@ -8,18 +9,22 @@ $(function () {
     const signupX = $('.signup-x');
     const loginX = $('.login-x');
 
+    // Modal signup pop up when clicked.
     signupBtn.on("click", () => {
         signupModal.addClass("display");
     });
 
+    // Modal login pop when clicked.
     loginBtn.on("click", () => {
         loginModal.addClass("display");
     });
 
+    // Close signup modal.
     signupX.on("click", () => {
         signupModal.removeClass("display");
     });
 
+    // Close login modal.
     loginX.on("click", () => {
         loginModal.removeClass("display");
     });
