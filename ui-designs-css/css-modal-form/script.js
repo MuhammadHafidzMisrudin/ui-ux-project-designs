d0b337cf500b0be9e9c98190b809fde35c818a59
Hafidz <halfpheez92@gmail.com>
2019-05-21 17:11:40 +0800

    added jquery implementation for modals functionality.

--- ui-designs-css/css-modal-form/script.js
+++ ui-designs-css/css-modal-form/script.js
@@ -1,1 +1,26 @@
 // JS & Jquery implementations.
+
+$(function () {
+    const signupModal = $('.signup-form-wrapper');
+    const loginModal = $('.login-form-wrapper');
+    const signupBtn = $('.signup-btn');
+    const loginBtn = $('.login-btn');
+    const signupX = $('.signup-x');
+    const loginX = $('.login-x');
+
+    signupBtn.on("click", () => {
+        signupModal.addClass("display");
+    });
+
+    loginBtn.on("click", () => {
+        loginModal.addClass("display");
+    });
+
+    signupX.on("click", () => {
+        signupModal.removeClass("display");
+    });
+
+    loginX.on("click", () => {
+        loginModal.removeClass("display");
+    });
+});
