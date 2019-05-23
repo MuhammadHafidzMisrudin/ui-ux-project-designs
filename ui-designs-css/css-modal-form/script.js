d7320151e6c0343ac506c0c4c788f4829aaa1051
Hafidz <halfpheez92@gmail.com>
2019-05-23 15:36:15 +0800

    updated code, added disable dark overlay for modals and smooth transition.

--- ui-designs-css/css-modal-form/script.js
+++ ui-designs-css/css-modal-form/script.js
@@ -8,24 +8,29 @@ $(function () {
     const loginBtn = $('.login-btn');
     const signupX = $('.signup-x');
     const loginX = $('.login-x');
+    const formContainerDarkOverlay = $('.form-container');
 
     // Modal signup pop up when clicked.
     signupBtn.on("click", () => {
         signupModal.addClass("display");
+        formContainerDarkOverlay.addClass("disable-overlay");
     });
 
     // Modal login pop when clicked.
     loginBtn.on("click", () => {
         loginModal.addClass("display");
+        formContainerDarkOverlay.addClass("disable-overlay");
     });
 
     // Close signup modal.
     signupX.on("click", () => {
         signupModal.removeClass("display");
+        formContainerDarkOverlay.removeClass("disable-overlay");
     });
 
     // Close login modal.
     loginX.on("click", () => {
         loginModal.removeClass("display");
+        formContainerDarkOverlay.removeClass("disable-overlay");
     });
 });
