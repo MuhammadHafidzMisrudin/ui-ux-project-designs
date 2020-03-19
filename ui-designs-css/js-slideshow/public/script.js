bf76e07a59fabf0eb280f236a570c61c493f2429
Hafidz <halfpheez92@gmail.com>
2020-03-19 17:23:51 +0800

    updated changeslides function.

--- ui-designs-css/js-slideshow/public/script.js
+++ ui-designs-css/js-slideshow/public/script.js
@@ -3,6 +3,7 @@ $(() => {
     // indicate the number of a slide which will be currently showing.
     let current = 1;
 
+    // function to change slides automatically
     const changeSlides = () => {
 
         // select all elements with classname, slide and returns an array like object called node list.
@@ -12,6 +13,15 @@ $(() => {
         // transform a node list into an array.
         const slides = Array.from(slideList);
         console.log(slides);
+
+        // loop through array
+        slides.forEach((slide) => {
+            if (slide.classList[1].split("-")[1]*1 === current) {
+                slide.style.cssText = "visibility: visible; opacity: 1";
+            } else {
+                slide.style.cssText = "visibility: hidden; opacity: 0";
+            }
+        });
     };
 
     changeSlides();
