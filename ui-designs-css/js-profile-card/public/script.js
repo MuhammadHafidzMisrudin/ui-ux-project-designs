87fdf85b43121f9af2e7736be8552058e919fb9d
Hafidz <halfpheez92@gmail.com>
2020-05-15 16:44:50 +0800

    added desc of authoring styles for developer profile card.

--- ui-designs-css/js-profile-card/public/script.js
+++ ui-designs-css/js-profile-card/public/script.js
@@ -1,3 +1,9 @@
+/* 
+    Author: Muhammad Hafidz Misrudin
+    Description: Proprietary (no permission for copy or redistribution)
+    Date written: 22/04/2020
+    Date last updated: 15/05/2020
+*/
 $(() => {
     console.log("testing-jquery-works");
 
